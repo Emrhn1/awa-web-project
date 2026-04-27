a5b0c8ac754c5aa7969c7db06485068590e50937
Emirhan <emirhanerkn@hotmail.com>
2026-04-27 18:35:01 +0300

    Add Accident model with methods to retrieve all accidents and by ID

--- src/Models/Accident.php
+++ src/Models/Accident.php
@@ -1,2 +1,38 @@
 <?php
 
+require_once __DIR__ . '/../Config/database.php';
+
+class Accident {
+
+    public static function getAll(array $filters = []): array {
+        $pdo = getDB();
+        $sql = 'SELECT * FROM accidents WHERE 1=1';
+        $params = [];
+
+        if (!empty($filters['date'])) {
+            $sql .= ' AND date = :date';
+            $params[':date'] = $filters['date'];
+        }
+        if (!empty($filters['city'])) {
+            $sql .= ' AND city = :city';
+            $params[':city'] = $filters['city'];
+        }
+        if (!empty($filters['severity'])) {
+            $sql .= ' AND severity = :severity';
+            $params[':severity'] = $filters['severity'];
+        }
+
+        $sql .= ' ORDER BY date DESC, time DESC';
+
+        $stmt = $pdo->prepare($sql);
+        $stmt->execute($params);
+        return $stmt->fetchAll();
+    }
+
+    public static function getById(int $id): array|false {
+        $pdo = getDB();
+        $stmt = $pdo->prepare('SELECT * FROM accidents WHERE id = :id');
+        $stmt->execute([':id' => $id]);
+        return $stmt->fetch();
+    }
+}
