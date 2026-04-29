22b0b1cd7260bc14d01e60f3f231530172cda9f6
Emirhan <emirhanerkn@hotmail.com>
2026-04-29 17:32:44 +0300

    Format routes for accident API methods for improved readability

--- src/routes.php
+++ src/routes.php
@@ -4,6 +4,9 @@ require_once __DIR__ . '/Controllers/AccidentsController.php';
 
 const ROUTES = [
     'accidents' => [
-        'GET' => ['AccidentsController', 'handleGet'],
+        'GET'    => ['AccidentsController', 'handleGet'],
+        'POST'   => ['AccidentsController', 'handlePost'],
+        'PUT'    => ['AccidentsController', 'handlePut'],
+        'DELETE' => ['AccidentsController', 'handleDelete'],
     ],
 ];
